09ae159748c6b04783857b22b4c71e8cb9f13e38
nahuelbrizu <nahuelbrizu@outlook.com>
2023-12-04 18:42:31 -0300

    fix omniauth2

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,40 +3,24 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :google_oauth2
 
   def google_oauth2
-    Rails.logger.debug("Google OAuth Callback initiated")
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if request.env['omniauth.error'] == 'access_denied'
-      Rails.logger.error("User denied access. Redirecting to sign-in...")
-      redirect_to new_user_session_path, alert: 'Access was denied.'
-      return
-    end
-
-    begin
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-
-      if @user.persisted?
-        puts "User persisted. Redirecting..."
-        sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
-      else
-        puts "User not persisted. Redirecting to sign-in..."
-        session['google_data'] = request.env['omniauth.auth'].except(:extra)
-        redirect_to new_user_session_path, alert: @user.errors.full_messages.join("\n")
-      end
-    rescue => e
-      Rails.logger.error("Error during Google OAuth authentication: #{e.message}")
-      redirect_to new_user_session_path, alert: 'An error occurred during authentication.'
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"].except("extra")
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
 
-  #def failure
-  #  Rails.logger.error("Authentication failure")
+  def failure
+    redirect_to root_path, alert: "Authentication failed, please try again."
+  end
 
-  # redirect_to new_user_session_path
-  #  end
+  private
 
-
-  def passthru
-   render status: 404, plain: "Not found. Authentication passthru."
- end
+  def auth
+    auth = request.env['omniauth.auth']
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook google_oauth2]
+  validates :uid, uniqueness: { scope: :provider }
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.full_name = auth.info.name
-      user.avatar_url = auth.info.image
+      user.name = auth.info.name
+      user.password = Devise.friendly_token[0,20]
+      user.avatar = auth.info.image
+      user.skip_confirmation!
     end
   end
 
